95e2c8730b4f7a39acb980f06f8b33f7a3a64f99
François Jacquet <francoisjacquet@users.noreply.github.com>
2017-05-17 15:49:48 -0500

    Fix Instagram post regression: add URL to post content for automatic embedding.

--- model/model-psmb-instagram-cpt.php
+++ model/model-psmb-instagram-cpt.php
@@ -331,8 +331,9 @@ class Premise_Social_Media_Blogger_Instagram_CPT {
 			$post_type = 'psmb_instagram_' . $this->instance_id;
 		}
 
+		// Add Instagram video URL to post content for automatic embedding.
 		// Regular Post: only description.
-		$content = $photo_details['description'];
+		$content = $photo_details['url'] . "\r\n" . $photo_details['description'];
 
 		$post_status = 'publish';
 
@@ -341,7 +342,6 @@ class Premise_Social_Media_Blogger_Instagram_CPT {
 			$post_status = 'pending';
 		}
 
-		// Add Instagram video URL (in place of description) to post content for automatic embedding.
 		// Insert new Instagram post.
 		$instagram = array(
 			'post_title' => $photo_details['title'],
